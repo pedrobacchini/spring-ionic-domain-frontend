17a51da5d7eafed83031072b501b59d5a719b92f
Pedro Bacchini <pedrobacchini@outlook.com>
2020-02-12 23:48:36 -0300

    Handler errors 401 and default error with alert.

--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -2,11 +2,14 @@ import { Injectable } from "@angular/core";
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(public storageService:StorageService) {
+  constructor(
+    public storageService: StorageService,
+    public alertController: AlertController) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -21,14 +24,38 @@ export class ErrorInterceptor implements HttpInterceptor {
           case 403:
             this.handle403();
             break;
+          case 401:
+            this.handle401();
+            break;
+          default:
+            this.handlerDefaultError(errorObj);
+            break;
         }
 
         return Observable.throw(errorObj);
       }) as any;
   }
 
-  handle403() {
+  private handle403() {
     this.storageService.removeUserSession();
+  }
+
+  private handle401() {
+    this.alertController.create({
+      title: 'Error 401: Authentication failure',
+      message: 'Invalid email or password',
+      enableBackdropDismiss: false,
+      buttons: [{text: 'OK'}]
+    }).present();
+  }
+
+  private handlerDefaultError(error: any) {
+    this.alertController.create({
+      title: `Error ${error.status}: ${error.error}`,
+      message: error.message,
+      enableBackdropDismiss: false,
+      buttons: [{text: 'OK'}]
+    }).present();
   }
 }
 
